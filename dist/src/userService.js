fe41050feed9557b1f84c0de6e5fcbe4af9a049a
José Pedro Ribeiro <jose@nefertiti.tech>
2023-05-22 17:53:38 +0100

    remove modules

--- dist/src/userService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUser = void 0;
-function getUser(username) {
-    return `Retrieving ${username} from Github`;
-}
-exports.getUser = getUser;
-//# sourceMappingURL=userService.js.map
